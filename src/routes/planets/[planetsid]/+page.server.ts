2bd7c687cbefd397d4a1e235747ba6e044973c15
Danial-Doskuzhinov <doskuzhinovdanial@gmail.com>
2024-06-06 20:25:01 +0500

    env

--- src/routes/planets/[planetsid]/+page.server.ts
+++ src/routes/planets/[planetsid]/+page.server.ts
@@ -4,11 +4,7 @@ import {BASE_URL} from '$env/static/private';
 export const load: PageServerLoad = async ({ params }) => {
 try {
     const { planetsid } = params; 
-<<<<<<< HEAD
     const response = await fetch(`${BASE_URL}${planetsid}`);
-=======
-    const response = await fetch(`http://localhost:8081/api/v1/planets/${planetsid}`);
->>>>>>> df3725b675458ba3be215258dc9871bdda8c1113
      
     if (!response.ok) {
         console.error(`response dont ok  ${planetsid}. Status: ${response.status}`);
